c040d33fd939c3d3167a17d63e81784dc3752869
Nauman- <103512145+niveaaa@users.noreply.github.com>
2023-11-04 08:19:22 +0530

    Update main.py

--- eye-buddy/py/main.py
+++ eye-buddy/py/main.py
@@ -5,15 +5,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-# Load the pre-trained model and labels
 model = load_model("keras_model.h5", compile=False)
 class_names = [line.strip() for line in open("labels.txt", "r")]
 
 def classify_image():
-    # Open a file dialog for image selection
     file_path = filedialog.askopenfilename()
     if file_path:
-        # Load and preprocess the selected image
         image = Image.open(file_path).convert("RGB")
         image = ImageOps.fit(image, (224, 224), Image.LANCZOS)
         image_array = np.asarray(image)
@@ -21,26 +18,36 @@ def classify_image():
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
         data[0] = normalized_image_array
 
-        # Predict the image class
         prediction = model.predict(data)
         index = np.argmax(prediction)
         class_name = class_names[index]
         confidence_score = prediction[0][index]
 
-        # Update the result label
         result_label.config(text=f"Class: {class_name[2:]}\nConfidence Score: {confidence_score:.2f}")
 
-# Create the tkinter window
 window = tk.Tk()
-window.title("Image Classifier")
+window.title(" Eye-Buddy | Eye Care Because We Care")
 
-# Create a button to classify the image
-classify_button = tk.Button(window, text="Classify Image", command=classify_image)
-classify_button.pack()
+window.geometry("400x200")
+window.eval('tk::PlaceWindow . center')
 
-# Create a label to display the result
-result_label = tk.Label(window, text="", wraplength=400)
+classify_button = tk.Button(
+    window,
+    text="Upload Fundus Image",
+    command=classify_image,
+    bg="#5aedeb",  
+    fg="white",    
+    font=("Josefin Sans", 14)  
+)
+classify_button.pack(pady=20)  
+
+result_label = tk.Label(
+    window,
+    text="Diabetic Retinopathy Detection",
+    font=("Josefin Sans", 14),
+    wraplength=350,  
+    justify="left" 
+)
 result_label.pack()
 
-# Run the tkinter main loop
 window.mainloop()
