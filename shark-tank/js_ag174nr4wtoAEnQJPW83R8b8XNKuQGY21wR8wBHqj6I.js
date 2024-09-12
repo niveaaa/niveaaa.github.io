41cde76ea333210ca568667a8305d312529688c1
Nauman- <103512145+niveaaa@users.noreply.github.com>
2024-09-12 14:55:14 +0530

    Delete shark-tank directory

--- shark-tank/js_ag174nr4wtoAEnQJPW83R8b8XNKuQGY21wR8wBHqj6I.js
+++
@@ -1,8 +0,0 @@
-window.cb = window.cb || {};
-cb.core = cb.core || {};
-cb.core.utils = cb.core.utils || {};
-cb.core.utils.DeploymentProfile = cb.core.utils.DeploymentProfile || {};
-cb.core.utils.DeploymentProfile.iam = cb.core.utils.DeploymentProfile.iam || {};
-cb.core.utils.DeploymentProfile.iam.aws = cb.core.utils.DeploymentProfile.iam.aws || {};
-cb.core.utils.DeploymentProfile.iam.aws.cbAWSDomains = cb.core.utils.DeploymentProfile.iam.aws.cbAWSDomains || [];
-;
